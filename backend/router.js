baaac66e38e8268c6c013d492439e52eef80380c
srafi1 <shakilrafi0@gmail.com>
2020-01-21 20:32:46 -0500

    subscribe to orgs and get events

--- backend/router.js
+++ backend/router.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const expressSession = require('express-session');
 const User = require('./models/userModel');
+const Event = require('./models/eventModel');
+const Organization = require('./models/organizationModel');
 const { getUser, registerUser } = require('./auth');
 
 const router = express.Router();
@@ -22,6 +24,16 @@ function allExist(...items) {
     }
   }
   return true;
+}
+
+async function requireLogin(req, res, next) {
+  if (!req.session.email) {
+    res.json({success:false, message:'Must be logged in.'});
+  } else {
+    let user = await User.findOne({email: req.session.email});
+    req.user = user;
+    return next();
+  }
 }
 
 // register user and store email in session if successful
@@ -66,6 +78,39 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy();
   res.json({success: true});
+});
+
+router.get('/events', requireLogin, async (req,res) => {
+  let events = [];
+  for (let i in req.user.subscriptions) {
+    let org = req.user.subscriptions[i];
+    let newEvents = await Event.find({organization:org});
+    events = events.concat(newEvents);
+  }
+  res.json({success:true, events:events});
+});
+
+router.post('/subscribe', requireLogin, async (req, res) => {
+  let orgName = req.body.name;
+  if (!orgName) {
+    res.json({success:false, message:'No organization specified.'});
+  } else {
+    let orgNames = req.user.subscriptions.map(org => org.name);
+    if (orgNames.includes(orgName)) {
+      res.json({success:false,
+        message:'You are already subscribed to that organization.'});
+    } else {
+      let org = await Organization.findOne({name:orgName});
+      if (!org) {
+        res.json({success:false,
+          message:'That organization does not exist.'});
+      } else {
+        req.user.subscriptions.push(org);
+        await req.user.save();
+        res.json({success:true});
+      }
+    }
+  }
 });
 
 module.exports = router;

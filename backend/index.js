3436c72551b32a92c306108d0fc456c5e023329f
srafi1 <shakilrafi0@gmail.com>
2020-01-30 21:59:40 -0500

    renamed omni to town square in backend

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,21 @@ const router = require('./router');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/omni', {
+mongoose.connect('mongodb://localhost:27017/town-square', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.use('/api', router);
 
-let startMessage = `Omni API running on port ${PORT}`;
+let startMessage = `Town Square API running on port ${PORT}`;
 
 // following code is for running frontend and backend on same port for prod
 if (process.env.DEPLOY === 'prod') {
   let staticServe = express.static(path.join(__dirname, '../frontend/build'));
   app.use("/", staticServe);
   app.use("*", staticServe);
-  startMessage = `Omni web app running on port ${PORT}`;
+  startMessage = `Town Square web app running on port ${PORT}`;
 }
 
 app.listen(PORT, () => console.log(startMessage));
